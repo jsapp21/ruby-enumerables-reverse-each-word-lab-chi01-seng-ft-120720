06ec3e1c5961336804157a411a4667d00f9ea195
James Sapp <jameseisapp@gmail.com>
2020-11-18 18:22:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 # require 'pry'
-# 
+ 
+  # result = my_array.reverse
+  # final = result.join(' ') 
 
 def reverse_each_word(sentence1)
   my_array = sentence1.split
-  # result = my_array.reverse
-  # final = result.join(' ') 
+
   
 
 end
