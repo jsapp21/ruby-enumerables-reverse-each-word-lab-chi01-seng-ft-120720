77a36f3f1ae72f8ce0b9c6cca0a423f194936e0b
James Sapp <jameseisapp@gmail.com>
2020-11-18 19:59:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,15 +5,11 @@ require 'pry'
 
 def reverse_each_word(sentence1)
   my_array = sentence1.split
-  new_array = []
-  
-  test = my_array.collect do |name|
-    name.reverse 
-  end 
+
   test_two = my_array.each do |name|
     name.reverse 
   end
-  binding.pry  
+  test_two.join(' ')  
 end
 
  
